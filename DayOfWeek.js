5425ba25cf3239eb5099380d2452dc8fd1e8c925
Administrator <siddhiseth3009@gmail.com>
2020-11-18 01:52:50 +0530

    [Siddhi]Add. DayOfWeek.js added

---
+++ DayOfWeek.js
@@ -0,0 +1,35 @@
+process = require('process');
+console.log("Enter a number for day of the week");
+let input= process.argv[2];
+if(input==0)
+{
+    console.log("Sunday");
+}
+else if(input==1)
+{
+    console.log("Monday");
+}
+else if(input==2)
+{
+    console.log("Tuesday");
+}
+else if(input==3)
+{
+    console.log("Wednesday");
+}
+else if(input==4)
+{
+    console.log("Thursday");
+}
+else if(input==5)
+{
+    console.log("Friday");
+}
+else if(input==6)
+{
+    console.log("Saturday");
+}
+else
+{
+    console.log("Invalid Input");
+}
