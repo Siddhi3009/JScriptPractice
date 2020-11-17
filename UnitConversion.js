07cc70b53fcaefb4ebe08db743e8257e41ac0495
Administrator <siddhiseth3009@gmail.com>
2020-11-17 20:02:55 +0530

    [Siddhi]Refactor. UnitConversion.js refactored for 25 plots

--- UnitConversion.js
+++ UnitConversion.js
@@ -1,6 +1,3 @@
-let length = Math.floor(Math.random() * 99);
-let breadth = Math.floor(Math.random() * 99);
-console.log("Length of plot in ft " + length + " Breadth of plot in ft " + breadth);
 function GetArea(length, breadth)
 {
     length = length/12;
@@ -8,5 +5,11 @@ function GetArea(length, breadth)
     console.log("Length of plot in m " + length + " Breadth of plot in m " + breadth);
     return length*breadth;
 }
-let area = GetArea(length, breadth);
-console.log("Area in metre sq: " + area);
+for(let plotNo = 0; plotNo< 25; plotNo++)
+{
+    let length = Math.floor(Math.random() * 99);
+    let breadth = Math.floor(Math.random() * 99);
+    console.log("Length of plot in ft " + length + " Breadth of plot in ft " + breadth);
+    let area = GetArea(length, breadth);
+    console.log("Area in metre sq: " + area);
+}
